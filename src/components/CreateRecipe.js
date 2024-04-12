c39b5a3d82e7062bd3e7431ba8485118264ba738
Hammadmacho7 <hammad.musakhel@ug.bilkent.edu.tr>
2024-04-13 02:30:34 +0500

    Updated

--- src/components/CreateRecipe.js
+++ src/components/CreateRecipe.js
@@ -1,9 +1,68 @@
 import React from 'react'
+import {Form, Button} from 'react-bootstrap'
+import {useForm} from 'react-hook-form'
 
 export default function CreateRecipe() {
+
+  const {register, handleSubmit, reset, formState:{errors}} = useForm()
+  const [show, setShow] = useState(false)
+
+  const createRecipe =(data) => {
+
+    console.log(data)
+
+    const token = localStorage.getItem('REACT_TOKEN_AUTH_KEY');
+    console.log(token)
+
+    const requestOptions = {
+      method: 'POST',
+      headers:{
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${JSON.parse(token)}`
+      },
+      body:JSON.stringify(data) //to JSON
+    }
+  
+
+    fetch('/recipe/recipes', requestOptions)
+    .then(res => res.json())
+    .then(data => 
+      reset()
+    )
+    .catch(err=>console.log(err))
+  }
+
+
   return (
-    <div className='createRecipe'>
-      <h1>Create Recipe Page</h1>
+    <div className='container'>
+      <h1 className='heading'>Create A Recipe</h1>
+      <form>
+        <Form.Group>
+          <Form.Label className='makewhite'>Title</Form.Label>
+          <Form.Control type="text"
+              {...register('title', {required: true, maxLength:25})}
+              />
+        </Form.Group>
+        {errors.title && <p style={{ color: 'red' }}><small>Title is required</small></p>}
+        {errors.title?.type === "maxLength" && <p style={{ color: 'red' }}>
+            <small>Title should be less than 25 characters</small>
+        </p>}
+        <Form.Group>
+          <Form.Label className='makewhite'>Description</Form.Label>
+          <Form.Control as = "textarea" rows={5}
+              {...register('description', {required: true, maxLength:255})}
+              />
+        </Form.Group>
+        {errors.description && <p style={{ color: 'red' }}><small>Description is required</small></p>}
+        {errors.description?.type === "maxLength" && <p style={{ color: 'red' }}>
+           <small>Description should be less than 255 characters</small>
+        </p>}
+        <br></br>
+        <Form.Group>
+          <Button variant="primary" onClick={handleSubmit(createRecipe)}>Submit</Button>
+        </Form.Group>
+      </form>
     </div>
   )
 }
+
